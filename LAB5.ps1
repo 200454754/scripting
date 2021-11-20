e701445f4f17113466f57f0f537402cd17e08632
200454754 <94725731+200454754@users.noreply.github.com>
2021-11-19 20:05:48 -0500

    Add files via upload

---
+++ LAB5.ps1
@@ -0,0 +1,21 @@
+﻿Param ([switch]$System, 
+       [switch]$Disks, [switch]$Network)
+
+import-module jaspreet
+
+if ($System -eq $false -and $Disks -eq $false -and $Network -eq $false) {
+	echo "Uses functions in previous labs as a module for this perticular lab."
+	echo "Add Param arguments to get more specific results."
+     jaspreet-System;
+     jaspreet-Disks;jaspreet-Network;
+} else{
+     if ($System) {
+         jaspreet-System;
+     }
+     if ($Disks) {
+         jaspreet-Disks;
+     }
+     if ($Network) {
+         jaspreet-Network;
+     }
+}
